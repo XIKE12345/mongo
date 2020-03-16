6b7b3bd60fc9dbcf182b4c6c90078d0c301ae56f
huike <754873891@qq.com>
2020-03-16 10:26:39 +0800

    增加日志，增加@Scheduled，自动统计

--- src/main/java/com/jieyun/mongo/MongoApplication.java
+++ src/main/java/com/jieyun/mongo/MongoApplication.java
@@ -2,8 +2,10 @@ package com.jieyun.mongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MongoApplication {
 
     public static void main(String[] args) {
